9e6a340be649e70452c03e90d0e97a41f1accc0d
nicolasbeltrami <nicolas.beltrami@gmail.com>
2019-12-02 00:05:47 -0300

    commit

---
+++ app/src/main/java/com/nicolas/practica_07/Activities/MainActivity.java
@@ -0,0 +1,59 @@
+package com.nicolas.practica_07.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.nicolas.practica_07.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences prefs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        prefs = getSharedPreferences("Preference", Context.MODE_PRIVATE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_logout:
+                logout();
+                return true;
+            case R.id.menu_forget_logout:
+                removeSharedPreferences();
+                logout();
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void logout(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    private void removeSharedPreferences(){
+        prefs.edit().clear().apply();
+    }
+
+}
